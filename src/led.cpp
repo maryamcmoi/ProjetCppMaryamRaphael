89f3c08402ddb9cdd06e8c38421710fa91fc25a1
maryamcmoi <56129339+maryamcmoi@users.noreply.github.com>
2022-05-11 20:23:32 +0200

    Update led.cpp

--- src/led.cpp
+++ src/led.cpp
@@ -3,9 +3,8 @@
 
 
 #define PIN_LED1 D5
-#define PIN_LED2 D6//a changer 
+#define PIN_LED2 D6
 #define PIN_LED3 D7
-//#define PIN_LED4 D7
 #define BRIGHTNESS 50
 
 //constructeur 
@@ -47,11 +46,6 @@ led::led(int lane) {
       Serial.flush();
       this->pin = PIN_LED3;
       break;
-    //case (4): 
-      //Serial.println("lane 1 - pin D7"); 
-      //Serial.flush();
-      //this->pin = PIN_LED4;
-      //break;
     default : 
       Serial.println("no pin correspondance"); 
       Serial.flush(); 
@@ -90,12 +84,9 @@ int led::get_numero(){
         return this->lane;
 }
 void led::allumer_gagnant(char gagnant){
-    //int a = 1; //1 si attaquant est gagnant, 2 si défendeur est gagnant
-    //a++;
   int deb_coul; 
   int fin_coul; delay(500);
   //fonction volé : theatre chase rainbow 
- 
   for (int j = deb_coul; j < fin_coul; j++) {   // cycle all 256 colors in the wheel
       for (int q = 0; q < 3; q++) {
           for (uint16_t i = 0; i < this->pixels->numPixels(); i = i + 3) {
